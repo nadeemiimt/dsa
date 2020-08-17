1d6c20354ea5e4f0c7c50f0a6c57e6fdd3e23d69
mohdn <mohdn@vmware.com>
2020-08-18 01:13:10 +0530

    fixed mem and topdown issues

--- src/com/company/dp/LongestCommonSubsequence.java
+++ src/com/company/dp/LongestCommonSubsequence.java
@@ -11,7 +11,14 @@ public class LongestCommonSubsequence {
         {
             case 0: maxLength = lcsViaRecursion(a, b, a.length(), b.length());
                 break;
-            case 1: maxLength = lcsViaMemoization(a, b, a.length(), b.length());
+            case 1:
+                mem = new int[a.length()+1][b.length()+1];
+                for(int j = 0; j<=a.length(); j++){
+                    for(int k = 0; k<= b.length(); k++){
+                        mem[j][k] = -1;
+                    }
+                }
+                maxLength = lcsViaMemoization(a, b, a.length(), b.length());
                 break;
             case 2: maxLength = lcsViaTopDown(a, b, a.length(), b.length());
                 break;
@@ -39,6 +46,27 @@ public class LongestCommonSubsequence {
 
     public static int lcsViaMemoization(String a, String b, int l1, int l2){
         // base condition
+        if(l1 == 0 || l2 == 0)
+        {
+            return 0;
+        }
+
+        if(mem[l1][l2] != -1)
+        {
+            return mem[l1][l2];
+        }
+
+        // choice diagram
+        if(a.charAt(l1-1) == b.charAt(l2-1)){
+            return mem[l1][l2] = 1 + lcsViaMemoization(a, b, l1-1, l2-1);
+        }
+        else{
+            return mem[l1][l2] = Math.max(lcsViaMemoization(a, b, l1, l2-1), lcsViaMemoization(a, b, l1-1, l2));
+        }
+    }
+
+    public static int lcsViaTopDown(String a, String b, int l1, int l2){
+        // base condition
         mem = new int[l1+1][l2+1];
         for(int i = 0; i<=l1; i++){
             for(int j = 0; j<= l2; j++){
@@ -53,7 +81,7 @@ public class LongestCommonSubsequence {
             for(int j = 1; j<= l2; j++){
                 // choice diagram
                 if(a.charAt(i-1) == b.charAt(j-1)){
-                    return mem[i][j] = 1 + mem[i-1][j-1];
+                    mem[i][j] = 1 + mem[i-1][j-1];
                 }
                 else{
                     mem[i][j]  = Math.max(mem[i][j-1], mem[i-1][j]);
@@ -63,62 +91,4 @@ public class LongestCommonSubsequence {
 
         return mem[l1][l2];
     }
-
-    public static int lcsViaTopDown(String a, String b, int l1, int l2){
-        // base condition
-        mem = new int[l1+1][l2+1];
-        for(int i = 0; i<=l1; i++){
-            for(int j = 0; j<= l2; j++){
-                mem[i][j] = -1;
-            }
-        }
-        if(mem[l1][l2] != -1)
-        {
-            return mem[l1][l2];
-        }
-
-        // choice diagram
-        if(a.charAt(l1-1) == b.charAt(l2-1)){
-            return mem[l1][l2] = 1 + lcsViaRecursion(a, b, l1-1, l2-1);
-        }
-        else{
-            return mem[l1][l2] = Math.max(lcsViaRecursion(a, b, l1, l2-1), lcsViaRecursion(a, b, l1-1, l2));
-        }
-    }
-
-
-//    public static int maxProfitViaMemoization(int wt[], int val[], int cap, int size){
-//        // base condition
-//        if(mem[size][cap] != -1)
-//        {
-//            return mem[size][cap];
-//        }
-//
-//        // choice diagram
-//        if(wt[size-1] <= cap)
-//        {
-//            return mem[size][cap] = Math.max((val[size-1]+ maxProfitViaRecursion(wt, val, cap - wt[size-1], size -1)), maxProfitViaRecursion(wt, val, cap, size -1));
-//        }
-//
-//        return mem[size][cap] = maxProfitViaRecursion(wt, val, cap, size -1);
-//    }
-//
-//    public static int maxProfitViaTopDown(int wt[], int val[], int cap, int size){
-//        // choice diagram
-//        for(int i = 1; i <= size; i++)
-//        {
-//            for (int j = 1; j <= cap; j++)
-//            {
-//                if(wt[i-1] <= j)
-//                {
-//                    tab[i][j] = Math.max((val[i-1]+ tab[i-1][j-wt[i -1]]), tab[i-1][j]);
-//                }
-//                else{
-//                    tab[i][j] = tab[i-1][j];
-//                }
-//            }
-//        }
-//
-//        return tab[size][cap];
-//    }
 }
